16c0f9e9464e4f13bdec288ba03c3251f7fc0b4f
Mohsen Abdollahi <mohsen9april@gmail.com>
2020-07-15 11:34:48 +0430

    Initial Commit

---
+++ Travel App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Travel App
+//
+//  Created by Mohsen Abdollahi on 7/15/20.
+//  Copyright © 2020 Mohsen Abdollahi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
